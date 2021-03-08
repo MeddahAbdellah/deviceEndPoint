f788ac85835be00a3224f4c0593f49e19d503e54
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-08 23:31:45 +0100

    feat(signaling) added a ping system

--- public/device.js
+++ public/device.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-
+  let pingSignalInterval = null;
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const deviceName = urlParams.get('deviceName');
@@ -19,6 +19,7 @@ window.onload = function(){
       document.querySelector('video').srcObject = stream;
       socket.off();
       socket.on('listenSignal', (signalData) => {
+        clearInterval(pingSignalInterval);
         console.log('listenSignal', signalData.data);
         switch(signalData.type) { 
           case "refresh": 
@@ -47,7 +48,14 @@ window.onload = function(){
   initWebRtc(signalingSocket);
 
   devicesSocket.on('sessionInit', (data) => {
-    signalingSocket.emit('connectToSession', { sessionId: data.sessionId });   
+    signalingSocket.emit('connectToSession', { sessionId: data.sessionId });
+    pingSignalInterval = setInterval(() => {
+      signalingSocket.emit("sendSignal",{
+          sessionId: data.sessionId,
+          type: "initCall", 
+          sender: 'device',
+      }); 
+    }, 1000);
   });
 
 
