b0f2e48a4e73a2aaf5e7d34e2cb86ba56e201790
Abdallah Meddah <meddah.abdellah.spcx@gmail.com>
2021-03-08 20:10:14 +0100

    feat(streaming) added iceServers

--- public/device.js
+++ public/device.js
@@ -43,10 +43,26 @@ window.onload = function(){
 
   function handleRtcSignal(sessionId, socket, signalData, stream) { 
     if( signalData.data.type === "offer") {
+      const config = {
+          "iceServers": [
+            { "urls": "stun:stun.l.google.com:19302" },
+            { "urls": "stun:stun2.l.google.com:19302" },
+            { "urls": "stun:stun3.l.google.com:19302" },
+            
+              // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
+              // set your own servers here
+              {
+                  url: 'turn:192.158.29.39:3478?transport=udp',
+                  credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                  username: '28224511:1379330808'
+              }
+          ]
+      }
       rtcPeer =  new SimplePeer({
         initiator: false,
         trinkle: false,
         stream,
+        config
       });
       rtcPeer.removeAllListeners('signal');
       rtcPeer.removeAllListeners('error');
